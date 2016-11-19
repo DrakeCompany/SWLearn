ea5ffc2a29c0139e6b11fbbf04e1065f59462c25
kovacssandorUI <sandorkovacs122@gmail.com>
2016-11-19 11:34:49 +0100

    forum2 kész+órán manipuláció elkezdve

--- fe-base/UI/Js/forum2/app/script/index.js
+++ fe-base/UI/Js/forum2/app/script/index.js
@@ -1,3 +1,56 @@
 /**
  * Created by Sa on 2016.11.16..
  */
+var data = {
+    topics: [
+        {
+            title:'Quote of the day',
+            created: '09 10 2015 11:52:11 GMT+0100',
+            category: 'General',
+            brief: 'Work hard for what you want because it wont come to you without a fight. You have to be strong and courageous and know...',
+            views:10,
+            replay:1,
+            lastReplay:'09 11 2015 11:52:11 GMT+0100'
+        }, {
+            title:'lorum ipsum test by member',
+            created: '09 09 2015 11:45 AM GMT+0100',
+            category: 'General',
+            brief: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi...',
+            views:4,
+            replay:1,
+            lastReplay:'09 10 2015 11:45 AM GMT+0100'
+        }, {
+            title:'kitchen sink test',
+            created: '09 09 2015 11:40 AM GMT+0100',
+            category: 'General',
+            brief: 'This thread is to test the html tags and other related data',
+            views:3,
+            replay:1,
+            lastReplay:'09 10 2015 11:40 AM GMT+0100'
+        }, {
+            title:'food facts of plusealth',
+            created: '09 09 2015 11:31 AM GMT+0100',
+            category: 'Pictures',
+            brief: 'Food facts of plusHealth.com',
+            views:21,
+            replay:1,
+            lastReplay:'09 10 2015 11:31 AM GMT+0100'
+        }, {
+            title:'beatuiful videos',
+            created: '08 10 2015 18:29 AM GMT+0100',
+            category: 'Videos',
+            brief: 'The Most Beautiful Videos, That May Make You Cry',
+            views:2,
+            replay:1,
+            lastReplay:'08 11 2015 18:29 AM GMT+0100'
+        }, {
+            title:'man vs lion',
+            created: '08 10 2015 18:20 AM GMT+0100',
+            category: 'Videos',
+            brief: 'This is test video thread. The video id from youtube and the relative author contains the rights.',
+            views:2,
+            replay:1,
+            lastReplay:'08 11 2015 18:29 AM GMT+0100'
+        },
+    ]
+}
