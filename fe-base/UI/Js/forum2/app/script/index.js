a42a02505cfe1d5a36c24e75f854d2839c4b62a4
kovacssandorUI <sandorkovacs122@gmail.com>
2016-11-25 23:35:07 +0100

    atirva óra utan 1 popupra

--- fe-base/UI/Js/forum2/app/script/index.js
+++ fe-base/UI/Js/forum2/app/script/index.js
@@ -2,91 +2,38 @@
  * Created by Sa on 2016.11.16..
  */
 var data = {
-    topics: [
-        // {
-        //     title:'Quote of the day',
-        //     created: '09 10 2015 11:52:11 GMT+0100',
-        //     category: 'General',
-        //     brief: 'Work hard for what you want because it wont come to you without a fight. You have to be strong and courageous and know...',
-        //     views:10,
-        //     replay:1,
-        //     leftTime:0,
-        //     lastReplay:'09 11 2015 11:52:11 GMT+0100'
-        // }, {
-        //     title:'lorum ipsum test by member',
-        //     created: '09 09 2015 11:45 AM GMT+0100',
-        //     category: 'General',
-        //     brief: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi...',
-        //     views:4,
-        //     replay:1,
-        //     lastReplay:'09 10 2015 11:45 AM GMT+0100'
-        // }, {
-        //     title:'kitchen sink test',
-        //     created: '09 09 2015 11:40 AM GMT+0100',
-        //     category: 'General',
-        //     brief: 'This thread is to test the html tags and other related data',
-        //     views:3,
-        //     replay:1,
-        //     lastReplay:'09 10 2015 11:40 AM GMT+0100'
-        // }, {
-        //     title:'food facts of plusealth',
-        //     created: '09 09 2015 11:31 AM GMT+0100',
-        //     category: 'Pictures',
-        //     brief: 'Food facts of plusHealth.com',
-        //     views:21,
-        //     replay:1,
-        //     lastReplay:'09 10 2015 11:31 AM GMT+0100'
-        // }, {
-        //     title:'beatuiful videos',
-        //     created: '08 10 2015 18:29 AM GMT+0100',
-        //     category: 'Videos',
-        //     brief: 'The Most Beautiful Videos, That May Make You Cry',
-        //     views:2,
-        //     replay:1,
-        //     lastReplay:'08 11 2015 18:29 AM GMT+0100'
-        // }, {
-        //     title:'man vs lion',
-        //     created: '08 10 2015 18:20 AM GMT+0100',
-        //     category: 'Videos',
-        //     brief: 'This is test video thread. The video id from youtube and the relative author contains the rights.',
-        //     views:2,
-        //     replay:1,
-        //     lastReplay:'08 11 2015 18:29 AM GMT+0100'
-        // },
-    ]
+    topics: []
 };
 
 modalSee();
 
-var $template = $('.template');
+
+var source = $("#template").html();
+var template = Handlebars.compile(source);
 
 function render(response) {
     $('#leftMain').html('');
-
     response.forEach((topic)=> {
-        var $topic = $template.clone();
+
+        // var $topic = $template.clone();
+        var leftTimeUnit="Minutes";
         var leftTimeCount = Math.floor((new Date() - new Date(topic.created)) / 1000 / 60);
         if (leftTimeCount > 60) {
             leftTimeCount = Math.floor((new Date() - new Date(topic.created)) / 1000 / 60 / 60);
-            $topic.find('.leftTimeUnit').html(" Hours"); //.css('color','blue'); :-)
+            leftTimeUnit="Hours";
+            // $topic.find('.leftTimeUnit').html(" Hours"); //.css('color','blue'); :-)
             // document.getElementsByClassName("leftTimeUnit").innerHTML=" Hours";
             if (leftTimeCount > 24) {
                 leftTimeCount = Math.floor((new Date() - new Date(topic.created)) / 1000 / 60 / 60 / 24);
-                $topic.find('.leftTimeUnit').html(" Days");
+                // $topic.find('.leftTimeUnit').html(" Days");
+                leftTimeUnit="Days";
             }
-        }
-        $topic.removeClass('template');
-        $topic.find('.titleArticle').html(topic.title);
-        $topic.find('.created').html(topic.created);
-        $topic.find('.category').html(topic.category);
-        $topic.find('.brief').html(topic.brief);
-        $topic.find('.views').html(topic.views);
-        $topic.find('.replay').html(topic.replay);
-        $topic.find('.leftTime').html(leftTimeCount);
-        $topic.find('.lastReplay').html(topic.lastReplay);
-        $topic.find('.deleteTopic').data('id', topic.id);
-        $topic.find('.modButton').data('id', topic.id);
-        $('#leftMain').append($topic);
+        };
+        topic.leftTimeUnit = leftTimeUnit;
+        topic.leftTime=leftTimeCount;
+
+        var topicTemplate= template(topic);
+        $('#leftMain').append(topicTemplate);
     });
 }
 
@@ -100,21 +47,18 @@ function modalSee() {
 var searchId = 0;
 
 function modModalSee() {
-    $(document).on('click', '.modButton', function (event) {
+    $(document).on('click', '#modButton', function (event) {
         event.preventDefault();
         searchId = $(this).data('id');
+        console.log("searchId");
+        console.log(searchId);
 
-        $.get('./topics').then(function (response3) {
-            response3.forEach((topic)=> {
-                if (topic.id == searchId) {
-                    $('#modTitleInput').val(topic.title);
-                    $('#modCategory').val(topic.category);
-                    $('#modBrief').val(topic.brief);
-                }
-            })
-        });
-
-        $(".modModal").css('display', 'block');
+        $.get('./topics/'+searchId).then(function (response3) {
+            $('#titleInput').val(response3.title);
+            $('#category').val(response3.category);
+            $('#brief').val(response3.brief);
+        })
+        $(".modal").css('display', 'block');
     });
 };
 modModalSee();
@@ -185,15 +129,7 @@ function deleteActTopic() {
         event.preventDefault();
         var id = $(this).data('id');
         httpNewRequest('DELETE', '/topics/' + id);
-        // $.ajax({
-        //     method: 'DELETE',
-        //     url: '/topics/' + id
-        // }).then(function () {// thennel akkor fut le, amikor az az előtte lévő lefutott
-        //     $.get('./topics').then(function (response2) {
-        //         writeToConsole(response2);
-        //         render(response2);
-        //     });
-        // });
+
     })
 }
 deleteActTopic();
@@ -229,3 +165,7 @@ $.get('./topics').then(function (response2) {
     render(response2);
     sendNewTopic(response2);
 });
+
+console.log("handlebars");
+console.log(Handlebars);
+
